e9eb722df04173bab26776e986d5c37481ef9fee
Unkie <unknown920@gmail.com>
2024-05-05 21:24:13 +0300

    refactor: updated plan

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -10,9 +10,10 @@ import './globalCss/fonts.css';
 // TODO zhdi:
 // 1. Add Radix library for UI components
 // 2. Add Router
-// 3. Migrate to feature-based architecture
-// 4. Update all dependencies
-// 5. Migrate to TSC
+// 3. Add lefthook and set it up
+// 4. Migrate to feature-based architecture
+// 5. Update all dependencies
+// 6. Migrate to TSC
 
 export const App = () => (
   <Provider store={store}>
