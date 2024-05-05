a5586a07ba104aad2d8b825bcc98127ab2b9973f
Unkie <unknown920@gmail.com>
2024-05-05 21:22:27 +0300

    refactor: added plan to update projects

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,6 +7,13 @@ import { store } from './reduxProvider/index';
 import './globalCss/reset.css';
 import './globalCss/fonts.css';
 
+// TODO zhdi:
+// 1. Add Radix library for UI components
+// 2. Add Router
+// 3. Migrate to feature-based architecture
+// 4. Update all dependencies
+// 5. Migrate to TSC
+
 export const App = () => (
   <Provider store={store}>
     <CountWidget />
